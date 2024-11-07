e2f14fbd40f7b165c97544dce75c4e420c3408ec
Miryan Romero <miryamrolo10@gmail.com>
2024-11-07 10:32:44 -0500

    EliminarEditar

--- ScriptD.js
+++ ScriptD.js
@@ -20,3 +20,17 @@ function guardarRegistro() {
         alert("Llena los campos primero, si no, obvio no te va a guardar nada gil.");
     }
 }
+
+function eliminarRegistro(button) {
+    var miru = button.parentNode.parentNode;
+    miru.parentNode.removeChild(miru);
+}
+function editarRegistro(button) {
+    var miru = button.parentNode.parentNode;
+    document.getElementById("nombre").value = miru.cells[0].innerText;
+    document.getElementById("apellido").value = miru.cells[1].innerText;
+    document.getElementById("cedula").value = miru.cells[2].innerText;
+    document.getElementById("edad").value = miru.cells[3].innerText;
+
+    miru.parentNode.removeChild(miru);
+}
