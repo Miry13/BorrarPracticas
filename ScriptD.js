713fad5000d7977a66277b47f351ae4bbb28d92f
Miryan Romero <miryamrolo10@gmail.com>
2024-11-07 10:31:27 -0500

    GuardarMostrar

---
+++ ScriptD.js
@@ -0,0 +1,22 @@
+function guardarRegistro() {
+    var Nombres = document.getElementById("nombre").value;
+    var Apellidos = document.getElementById("apellido").value;
+    var Cedula = document.getElementById("cedula").value;
+    var Edad = document.getElementById("edad").value;
+
+    if (Nombres && Apellidos && Cedula && Edad) {
+        var table = document.getElementById("tablaCuerpo");
+        var miru = table.insertRow();
+        miru.innerHTML = `
+            <td>${Nombres}</td>
+            <td>${Apellidos}</td>
+            <td>${Cedula}</td>
+            <td>${Edad}</td>`;
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("cedula").value = "";
+        document.getElementById("edad").value = "";
+    } else {
+        alert("Llena los campos primero, si no, obvio no te va a guardar nada gil.");
+    }
+}
